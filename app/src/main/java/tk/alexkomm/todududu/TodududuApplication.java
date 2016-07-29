294152880fe9feb3a2e326ec2a1384aabdd36aec
alexkomm <akommv@gmail.com>
2016-07-29 13:44:43 +0500

    Added main activity, temporary removed connectedAndroidTest

---
+++ app/src/main/java/tk/alexkomm/todududu/TodududuApplication.java
@@ -0,0 +1,8 @@
+package tk.alexkomm.todududu;
+
+import android.app.Application;
+
+public class TodududuApplication extends Application {
+
+
+}
---
+++ app/src/main/java/tk/alexkomm/todududu/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package tk.alexkomm.todududu.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tk.alexkomm.todududu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
